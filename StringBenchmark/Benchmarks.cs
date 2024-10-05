1612a81c044ebdcbd979539ddffa402d4d673713
Roshan <Roshan@DESKTOP-SIMEP6O>
2024-10-05 20:59:58 +0530

    replace hard coded iteration to dynamic

--- StringBenchmark/Benchmarks.cs
+++ StringBenchmark/Benchmarks.cs
@@ -6,19 +6,23 @@ using BenchmarkDotNet.Attributes;
 
 namespace StringBenchmark
 {
+    [MemoryDiagnoser]
     public class Benchmarks
     {
+        [Params(5,50,500,5000)]
+        public int N { get; set; }
+
         [Benchmark (Baseline =true)]
         public string StringJoin()
         {
-            return string.Join(", ", Enumerable.Range(0,5).Select(i=> i.ToString()));
+            return string.Join(", ", Enumerable.Range(0,N).Select(i=> i.ToString()));
         }
 
         [Benchmark]
         public string StringBuilder()
         {
             var sb = new StringBuilder();
-            for(int i=0; i<5; i++)
+            for(int i=0; i<N; i++)
             {
                 sb.Append(i);
                 sb.Append(", ");
